17749bd8f858ce2d3c17e931d366652fbd7ad63e
syike20 <dashrathrao28@gmail.com>
2026-09-02 23:14:01 +0530

    Add Array Problems

--- Arrays/Remove_Duplicates_From_Sorted_Arrays.java
+++ Arrays/Remove_Duplicates_From_Sorted_Arrays.java
@@ -1,23 +1,38 @@
-import java.util.Scanner;
-public class Remove_Duplicates_From_Sorted_Arrays {
+import java.util.HashSet;
 
-    public static void BruteForce(int[]arr ,int n){
-        for(int i=0 ; i<n ;i++){
-            for(int j=1 ; j<n ; j++){
-                if(arr[j]==arr[i]){
-                    System.out.println(i);
-                }
+public class Remove_Duplicates_From_Sorted_Arrays {
+    public static void BruteForce(int[] arr){
+        HashSet <Integer> set = new HashSet<>();
+        int index = 0;
+        for(int element : arr){
+            if(!set.contains(element)){
+                set.add(element);
+                arr[index] = element;
+                index++;
             }
+        }
+        System.out.println("Array after removing elements : ");
+        for(int i=0 ; i<index ; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void Optimal(int[] arr,int n){
+        int i = 0 ;
+        for(int j= 1 ; j<n ; j++){
+            if(arr[j]!=arr[i]){
+                arr[i+1] = arr[j];
+                i++;
+            }
+        }
+        for(int k=0 ; k < i+1 ; k++){
+            System.out.print(arr[k] + " ");
         }
     }
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Size of Array ");
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-        for(int i=0 ; i<n ; i++){
-            arr[i] = sc.nextInt();
-        }
-        BruteForce(arr,n);
+        int[] arr = {1,1,2,2,2,3,3};
+        int n = arr.length;
+        BruteForce(arr);
+        Optimal(arr,n);
+    
     }
 }
